05f859a1f41cd85d294154b2e62d6306a18ab5b2
gin <73906888+gingkapls@users.noreply.github.com>
2024-04-05 22:35:46 +0530

    switch to classes, change card width

--- script.js
+++ script.js
@@ -1,3 +1,96 @@
+const bookList = [
+  {
+    author: "Chinua Achebe",
+    country: "Nigeria",
+    imageLink: "images/things-fall-apart.jpg",
+    language: "English",
+    link: "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
+    pages: 209,
+    title: "Things Fall Apart",
+    year: 1958,
+  },
+  {
+    author: "Hans Christian Andersen",
+    country: "Denmark",
+    imageLink: "images/fairy-tales.jpg",
+    language: "Danish",
+    link: "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
+    pages: 784,
+    title: "Fairy tales",
+    year: 1836,
+  },
+  {
+    author: "Dante Alighieri",
+    country: "Italy",
+    imageLink: "images/the-divine-comedy.jpg",
+    language: "Italian",
+    link: "https://en.wikipedia.org/wiki/Divine_Comedy\n",
+    pages: 928,
+    title: "The Divine Comedy",
+    year: 1315,
+  },
+  {
+    author: "Unknown",
+    country: "Sumer and Akkadian Empire",
+    imageLink: "images/the-epic-of-gilgamesh.jpg",
+    language: "Akkadian",
+    link: "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
+    pages: 160,
+    title: "The Epic Of Gilgamesh",
+    year: -1700,
+  },
+  {
+    author: "Unknown",
+    country: "Achaemenid Empire",
+    imageLink: "images/the-book-of-job.jpg",
+    language: "Hebrew",
+    link: "https://en.wikipedia.org/wiki/Book_of_Job\n",
+    pages: 176,
+    title: "The Book Of Job",
+    year: -600,
+  },
+  {
+    author: "Unknown",
+    country: "India/Iran/Iraq/Egypt/Tajikistan",
+    imageLink: "images/one-thousand-and-one-nights.jpg",
+    language: "Arabic",
+    link: "https://en.wikipedia.org/wiki/One_Thousand_and_One_Nights\n",
+    pages: 288,
+    title: "One Thousand and One Nights",
+    year: 1200,
+  },
+  {
+    author: "Unknown",
+    country: "Iceland",
+    imageLink: "images/njals-saga.jpg",
+    language: "Old Norse",
+    link: "https://en.wikipedia.org/wiki/Nj%C3%A1ls_saga\n",
+    pages: 384,
+    title: "Njell's Saga",
+    year: 1350,
+  },
+  {
+    author: "Jane Austen",
+    country: "United Kingdom",
+    imageLink: "images/pride-and-prejudice.jpg",
+    language: "English",
+    link: "https://en.wikipedia.org/wiki/Pride_and_Prejudice\n",
+    pages: 226,
+    title: "Pride and Prejudice",
+    year: 1813,
+  },
+  {
+    author: "Fyodor Dostoevsky",
+    country: "Russia",
+    imageLink: "images/crime-and-punishment.jpg",
+    language: "Russian",
+    link: "https://en.wikipedia.org/wiki/Crime_and_Punishment\n",
+    pages: 551,
+    title: "Crime and Punishment",
+    year: 1866,
+  },
+];
+
 const colorPalette = [
   "#FFEBEE",
   "#FCE4EC",
@@ -21,42 +114,35 @@ const colorPalette = [
 const generateRandomColor = () =>
   colorPalette.at(Math.floor(Math.random() * colorPalette.length));
 
-function Book(title, author, pages, read = false) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.color = generateRandomColor();
-}
+const Book = class {
+  constructor({ title, author, pages, read = false }) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.color = generateRandomColor();
+  }
 
-Book.prototype.info = function () {
-  // return `${this.title} by ${this.author}, ${this.pages} pages, ${
-  // this.read ? "read" : "not read yet"
-  // }`;
-
-  return {
-    title: this.title,
-    author: this.author,
-    pages: this.pages,
-    read: this.read,
+  info = () => {
+    return {
+      title: this.title,
+      author: this.author,
+      pages: this.pages,
+      read: this.read,
+    };
   };
+
+  toggleRead = () => (this.read = !this.read);
 };
 
-Book.prototype.toggleRead = function () {
-  this.read = !this.read;
-  return this.read;
-};
-
-const myLibrary = [
-  new Book("The Hobbit", "J.R.R Tolkien", 296, true),
-  new Book("The Rabbit", "R.R Man", 392, false),
-  new Book("The Wabbit", "M.R Falken", 255, false),
-  new Book("The Jammit", "J. Tol", 142, true),
-  new Book("The Jammit", "J. Tol", 142, true),
-  new Book("The Jammit", "J. Tol", 142, true),
-  new Book("The Jammit", "J. Tol", 142, true),
-  new Book("The Jammit", "J. Tol", 142, true),
-];
+const myLibrary = bookList.map((book) => {
+  return new Book({
+    title: book.title,
+    author: book.author,
+    pages: book.pages,
+    read: Math.random() > 0.5 ? true : false,
+  });
+});
 
 const createCard = (book, id) => {
   const div = document.createElement("div");
@@ -147,12 +233,12 @@ const addToLibrary = (event) => {
     return;
   }
 
-  const newBook = new Book(
-    (title = newFormData.get("bookTitle")),
-    (author = newFormData.get("bookAuthor")),
-    (pages = newFormData.get("bookPages")),
-    (read = newFormData.get("bookIsRead") === "true" ? true : false)
-  );
+  const newBook = new Book({
+    title: newFormData.get("bookTitle"),
+    author: newFormData.get("bookAuthor"),
+    pages: newFormData.get("bookPages"),
+    read: newFormData.get("bookIsRead") === "true" ? true : false,
+  });
   myLibrary.push(newBook);
   form.reset();
   dialogAddBook.close();
